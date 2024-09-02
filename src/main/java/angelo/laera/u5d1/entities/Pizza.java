634b772e6ce97a409fe0a0e966e5ae1d8aad4ab2
angelolaera <angelo.laera.1995@gmail.com>
2024-09-02 16:08:11 +0200

    Initial commit

---
+++ src/main/java/angelo/laera/u5d1/entities/Pizza.java
@@ -0,0 +1,59 @@
+package angelo.laera.u5d1.entities;
+
+import java.util.List;
+
+public class Pizza {
+    private String nome;
+    private List <Topping> toppings;
+    private int calorie;
+    private double prezzo;
+
+    public Pizza(String nome, List<Topping> toppings, int calorie, double prezzo) {
+        this.nome = nome;
+        this.toppings = toppings;
+        this.calorie = calorie;
+        this.prezzo = prezzo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(int calorie) {
+        this.calorie = calorie;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "nome='" + nome + '\'' +
+                ", toppings=" + toppings +
+                ", calorie=" + calorie +
+                ", prezzo=" + prezzo +
+                '}';
+    }
+}
---
+++ src/main/java/angelo/laera/u5d1/entities/Topping.java
@@ -0,0 +1,37 @@
+package angelo.laera.u5d1.entities;
+
+public class Topping {
+    private String nome;
+    private int calorie;
+    private double prezzo;
+
+    public Topping(String nome, int calorie, double prezzo) {
+        this.nome = nome;
+        this.calorie = calorie;
+        this.prezzo = prezzo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(int calorie) {
+        this.calorie = calorie;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+}
